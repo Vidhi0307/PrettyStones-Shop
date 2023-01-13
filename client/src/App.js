7289629d58a0f7aadabf3c0c321c626a76d94787
Akhilesh Sharma <vidhi.sydney07@gmail.com>
2023-01-14 01:22:18 +1100

    Started routing on front -end and fixed backend Graphql resolvers

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,38 @@
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { ChakraProvider } from "@chakra-ui/react";
+
+//pages and Components
+import Home from './pages/HomePage'
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <main className='p-3'><Container>
-        <h1>Welcome to Pretty Stones -Jewellery shop
-          </h1>
+    <ChakraProvider>
+      <div className="App">
+        <BrowserRouter>
+        <Header />
+        <div className="Pages">
+
+          <Routes>
+
+            <Route path= "/" element ={<Home/>}/>
+
+            
+            
+            </Routes>
+      
+        <main className="p-3">
+          <Container>
+           
           </Container>
         </main>
-      <Footer/>
-         </div>
+        <Footer />
+        </div>
+        </BrowserRouter>
+      </div>
+    </ChakraProvider>
   );
 }
 
